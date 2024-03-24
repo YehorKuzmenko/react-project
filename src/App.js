1c4be21ed14334162760e07a36e89f456afcf347
YehorKuzmenko <yehor.kuzmenko@etu.univ-amu.fr>
2024-03-24 14:50:50 +0100

    Adds Header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Header from './Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [],
+      filter: ''
+    };
+  }
+
+  countTasks() {
+
+      let total = this.state.tasks.length;
+      let done = this.state.tasks.filter(task => task.done).length;
+      let toDo = total - done;
+
+      return (
+          <div>
+            <p>Total tasks: {total}</p>
+            <p>Tasks done: {done}</p>
+            <p>Tasks to do: {toDo}</p>
+          </div>
+      );
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <Header tasks={this.countTasks()}/>
+          <div>Tasks</div>
+        </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+function Header({tasks}) {
+    return (
+        <div className="Header">
+                <header>
+                    {tasks}
+                </header>
+        </div>
+    );
+}
+
+export default Header;
