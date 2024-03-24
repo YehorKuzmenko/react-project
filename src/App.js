441f51903c820880c13b86d9ac0d6985db502b4c
YehorKuzmenko <yehor.kuzmenko@etu.univ-amu.fr>
2024-03-24 16:32:45 +0100

    Adds some cool things

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import './App.css';
 import React from 'react';
 import Header from './Header';
 import Footer from "./Footer";
-import Modal from './Modal';
 import ModalComponent from "./Modal";
+
+import DeleteIcon from '@mui/icons-material/DeleteOutline';
+import EditIcon from '@mui/icons-material/Edit';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 
 class App extends React.Component {
   constructor(props) {
@@ -35,6 +39,32 @@ class App extends React.Component {
           </div>
       );
   }
+
+    moveTaskUp = (index) => {
+        this.setState(prevState => {
+            if (index === 0) {
+                return prevState;
+            }
+            const tasks = [...prevState.tasks];
+            const temp = tasks[index];
+            tasks[index] = tasks[index - 1];
+            tasks[index - 1] = temp;
+            return { tasks };
+        });
+    }
+
+    moveTaskDown = (index) => {
+        this.setState(prevState => {
+            if (index === prevState.tasks.length - 1) {
+                return prevState;
+            }
+            const tasks = [...prevState.tasks];
+            const temp = tasks[index];
+            tasks[index] = tasks[index + 1];
+            tasks[index + 1] = temp;
+            return { tasks };
+        });
+    }
 
   handleSearchInput = (value) => {
       this.setState({search: value});
@@ -83,36 +113,51 @@ class App extends React.Component {
         }
     }
 
-  render() {
-      return (
-          <div className="App">
-              <Header tasks={this.countTasks()}/>
-              <ModalComponent
-                show={this.state.showModal}
-                handleClose={() => this.setState({showModal: false, currentTask: null})}
-                name={this.state.currentTask ? this.state.currentTask.name : ''}
-                handleAddUpdateTask={this.handleAddUpdateTask}
-              />
-              <div>Tasks</div>
-              <ul>
-                  {this.state.tasks
-                      .filter(task => this.state.search.length < 3 || task.name.includes(this.state.search))
-                      .map((task, index) => (
-                          <li key={index} className={task.done ? 'done' : ''}>
-                              <input type="checkbox" checked={task.done} onChange={() => this.toggleTasksDone(index)}></input>
-                              {task.name}
-                                <button onClick={() => this.setState({showModal: true, currentTask: {name: task.name, index}})}>Edit</button>
-                                <button onClick={() => this.deleteTask(index)}>Delete</button>
-                          </li>
-                      ))}
-              </ul>
-              <Footer
-                  showAddTask={() => this.setState({showModal: true})}
-                  handleSearchInput={this.handleSearchInput}
-              />
-          </div>
-      );
-  }
+    saveTasks = () => {
+        localStorage.setItem('tasks', JSON.stringify(this.state.tasks));
+    }
+
+    loadTasks = () => {
+        const tasks = localStorage.getItem('tasks');
+        if (tasks) {
+            this.setState({tasks: JSON.parse(tasks)});
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Header tasks={this.countTasks()}/>
+                <ModalComponent
+                    show={this.state.showModal}
+                    handleClose={() => this.setState({showModal: false, currentTask: null})}
+                    name={this.state.currentTask ? this.state.currentTask.name : ''}
+                    handleAddUpdateTask={this.handleAddUpdateTask}
+                />
+                <div>Tasks</div>
+                <ul>
+                    {this.state.tasks
+                        .filter(task => this.state.search.length < 3 || task.name.includes(this.state.search))
+                        .map((task, index) => (
+                            <li key={index} className={task.done ? 'done' : ''}>
+                                <input type="checkbox" checked={task.done} onChange={() => this.toggleTasksDone(index)}></input>
+                                {task.name}
+                                <ArrowUpwardIcon onClick={() => this.moveTaskUp(index)} />
+                                <ArrowDownwardIcon onClick={() => this.moveTaskDown(index)} />
+                                <EditIcon onClick={() => this.setState({showModal: true, currentTask: {name: task.name, index}})}>Edit</EditIcon>
+                                <DeleteIcon onClick={() => this.deleteTask(index)} />
+                            </li>
+                        ))}
+                </ul>
+                <Footer
+                    loadTasks={this.loadTasks}
+                    saveTasks={this.saveTasks}
+                    showAddTask={() => this.setState({showModal: true})}
+                    handleSearchInput={this.handleSearchInput}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,8 @@
-function Footer({handleSearchInput, showAddTask}) {
+function Footer({handleSearchInput, showAddTask, saveTasks, loadTasks}) {
   return (
     <footer>
+      <button onClick={saveTasks}>Save Tasks</button>
+      <button onClick={loadTasks}>Load Tasks</button>
       <input type="text" placeholder="Search" onChange={event => handleSearchInput(event.target.value)}></input>
       <button onClick={showAddTask}>Add Task</button>
     </footer>
